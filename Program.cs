98b37350537f45ec28bf192e4dd8ff279857111a
BogdanPecheritsa <ipz231_pbd@student.ztu.edu.ua>
2024-01-25 23:29:56 +0200

    replaced text output in Ukrainian to English

--- Program.cs
+++ Program.cs
@@ -1,24 +1,20 @@
 ﻿using Sana03;
-using System.Text;
-
-Console.OutputEncoding = Encoding.Unicode;
-Console.InputEncoding = Encoding.Unicode;
 
 int N;
 bool f;
 do
 {
-    Console.Write("Введіть кількість елементів масиву: ");
+    Console.Write("Enter the number of array elements: ");
     f = int.TryParse(Console.ReadLine(), out N);
     if (f == false)
-        Console.WriteLine("Помилка! Введіть число!");
+        Console.WriteLine("Error! Enter a number!");
 } while (!f);
 
 MyArray myArray = new MyArray(N);
 
-Console.WriteLine($"\nСума від’ємних елементів масиву: {myArray.SumNegative()}");
-Console.WriteLine($"Мінімальний елемент масиву: {myArray.MinElement()}");
-Console.WriteLine($"Індекс максимального елемента масиву: {myArray.MaxIndex()}");
-Console.WriteLine($"Максимальний за модулем елемент масиву: {myArray.MaxAbs()}");
-Console.WriteLine($"Сума індексів додатних елементів: {myArray.SumPositiveIndices()}");
-Console.WriteLine($"Кількість цілих чисел у масиві: {myArray.CountIntegers()}");
+Console.WriteLine($"\nThe sum of the negative elements of the array: {myArray.SumNegative()}");
+Console.WriteLine($"Minimum array element: {myArray.MinElement()}");
+Console.WriteLine($"The index of the maximum element of the array: {myArray.MaxIndex()}");
+Console.WriteLine($"The maximum array element by modulus: {myArray.MaxAbs()}");
+Console.WriteLine($"The sum of indices of positive elements: {myArray.SumPositiveIndices()}");
+Console.WriteLine($"The number of integers in the array: {myArray.CountIntegers()}");
