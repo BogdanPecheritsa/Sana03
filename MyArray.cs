9189320dcf1ff5c522675f96635a600e198b456b
BogdanPecheritsa <ipz231_pbd@student.ztu.edu.ua>
2024-01-25 21:20:40 +0200

    Add class MyArray.cs, implemented constructor MyArray with random filling of the array

---
+++ MyArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sana03
+{
+    public class MyArray
+    {
+        public int[] arr;
+
+        public MyArray(int count)
+        {
+            arr = new int[count];
+            Random rand = new();
+            for (int i = 0; i < count; i++)
+            {
+                arr[i] = rand.Next(-100, 100);
+            }
+        }
+    }
+}
